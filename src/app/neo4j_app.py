b90ab0967f66726858f6439d36c6d577db60751c
ShearmanChua <shearman_chua@hotmail.com>
2023-01-16 17:56:25 +0800

    happy chinese new year

---
+++ src/app/neo4j_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+from utils import neo4jManager
+
+
--- src/utils/neo4jManager.py
+++ src/utils/neo4jManager.py
@@ -78,34 +78,70 @@ class Neo4jManager():
     def delete_collection(self,collection_name: str):
         pass
 
-    def create_index(self, node_index_name: str, node_label: str, node_id: str, db=None):
-        with self.neo4j_conn.driver.session(database=db) if db is not None else self.neo4j_conn.driver.session() as session:
-            return session.run("CREATE INDEX {} IF NOT EXISTS FOR (n:{}) ON (n.{})".format(node_index_name, node_label, node_id))
+    def create_constraint(self, constraint_name: str, node_label: str, node_id: str, db=None):
+        '''Create a CONSTRAINT for a node type, so that there are
+           no replicated nodes. Requires a name for constraint(lowercase),
+           node_label(Uppercase) to be constrained, and the node_id to constraint on'''
 
-    def merge_node(self, node_labels, node_attributes, db=None):
+        with self.neo4j_conn.driver.session(database=db) if db is not None else self.neo4j_conn.driver.session() as session:
+            return session.run("CREATE CONSTRAINT {} IF NOT EXISTS ON (n:{}) ASSERT n.{} IS UNIQUE".format(constraint_name, node_label, node_id))
+
+    def merge_node(self, node_labels: List[str], node_attributes: dict, db=None):
+        ''' Create new nodes using th MERGE cypher query
+            The MERGE clause ensures that a pattern exists 
+            in the graph. Either the entire pattern already exists, 
+            or the entire pattern needs to be created. In this way,
+            it’s helpful to think of MERGE as attempting a MATCH on 
+            the pattern, and if no match is found, a CREATE of the pattern.
+
+            node_labels: a List of strings that define the node types of the node
+            node_attributes: attributes for the node being merged'''
+
         with self.neo4j_conn.driver.session(database=db) if db is not None else self.neo4j_conn.driver.session() as session:
             node_labels = ":".join(node_labels)
             node_attributes = "{"+", ".join([re.sub('[^A-Za-z0-9]+', '_', k)+" : '"+str(node_attributes[k]).replace(
                 "'", "").encode("ascii", "ignore").decode()+"'" for k in node_attributes.keys() if not k[0].isdigit()])+"}"
-            # print("MERGE (p:{} {}) RETURN p".format(node_label, node_attributes))'
-            print("MERGE (p:{} {}) RETURN p".format(node_labels, node_attributes))
-            print("\n")
-            return session.run("MERGE (p:{} {}) RETURN p".format(node_labels, node_attributes)).single().value()
 
-    def create_node(self, collection_name: str, nodes: List[dict]):
+            print("MERGE (n:{} {}) RETURN n".format(node_labels, node_attributes))
+            print("\n")
+
+            return session.run("MERGE (n:{} {}) RETURN n".format(node_labels, node_attributes)).single().value()
+
+    def create_node(self, node_collection: str, nodes: List[dict], node_id_field: str, node_labels: List[str] = []):
+        '''Create nodes given a List of dictionary of node attributes
+
+           node_collection: the node type of the node to be generated
+           nodes: List of nodes attributes in a dictionary
+           node_id_field: the key in the node attributes dict that is the node id
+           node_labels: any other node types to be tagged to the node'''
+
+        if node_labels:
+            node_labels = [label.capitalize() for label in node_labels]
+            node_labels.append(node_collection)
+            node_labels = list(set(node_labels))
+        else:
+            node_labels = [node_collection.capitalize()]
+
         for node in tqdm(nodes):
             node_attributes = node.copy()
             for key,node_attribute in node_attributes:
-                if type(node_attribute) == datetime:
+                if isinstance(node_attribute, datetime):
                     neo4j_datetime = DateTime(node_attribute.year, node_attribute.month, node_attribute.day, node_attribute.minute, node_attribute.second)
                     node_attributes.update({key, neo4j_datetime})
-            node_labels = [label.capitalize() for label in node['node_labels']]
-            node_id = node['node_id']
-            node_attributes['neo4j_collection'] = collection_name
-            self.merge_node(node_labels,node_attributes)
-            for label in node_labels:
-                self.create_index(label.lower()+'_index', label, node_id)
-    def merge_edge(self, source_node_label, source_node_attribute, target_node_label, target_node_attribute, relation_type, edge_attributes, db=None):
+
+            node_id = node[node_id_field]
+            node_attributes['node_id'] = node_id
+
+            node_attributes['node_collection'] = node_collection
+
+            self.merge_node(node_labels, node_attributes)
+
+        for label in node_labels:
+            self.create_constraint(label.lower(), label, node_id_field)
+
+    
+
+    def merge_edge_from_triples(self, source_node_label, source_node_attribute, target_node_label, target_node_attribute, relation_type, edge_attributes, db=None):
 
         with self.neo4j_conn.driver.session(database=db) if db is not None else self.neo4j_conn.driver.session() as session:
             source_attributes = "{"+", ".join([k+" : '"+str(source_node_attribute[k]).replace(
@@ -117,22 +153,28 @@ class Neo4jManager():
             # .single().value()
             return session.run("MATCH (s:{} {}), (t:{} {}) MERGE (s)<-[e:{} {}]-(t) RETURN e".format(source_node_label, source_attributes, target_node_label, target_attributes, relation_type, edge_attributes))
 
-    def generate_edges(self,collection_name: str, entities_triples: Union[pd.DataFrame,List[dict]], db=None):
+    def generate_edges_from_triples(self, collection_name: str, entities_triples: Union[pd.DataFrame,List[dict]], db=None):
+        ''''''
+
         if type(entities_triples) != pd.DataFrame:
             entities_triples = pd.json_normalize(entities_triples, max_level=0)
+
+        entities_triples['Subject'] = entities_triples.Subject.apply(lambda x: ast.literal_eval(str(x)))
+        entities_triples['Object'] = entities_triples.Object.apply(lambda x: ast.literal_eval(str(x)))
+
         for idx, triple in tqdm(entities_triples.iterrows(), total=len(entities_triples)):
             source_node_labels = triple['Subject']['node_labels']
             source_node_attributes = triple['Subject']
             for key,node_attribute in source_node_attributes:
-                if type(node_attribute) == datetime:
+                if isinstance(node_attribute, datetime):
                     neo4j_datetime = DateTime(node_attribute.year, node_attribute.month, node_attribute.day, node_attribute.minute, node_attribute.second)
                     source_node_attributes.update({key, neo4j_datetime})
             source_node_labels = [label.capitalize() for label in source_node_labels]
-            source_node_labels = triple['Subject']['node_labels']
 
+            target_node_labels = triple['Object']['node_labels']
             target_node_attributes = triple['Object']
             for key,node_attribute in target_node_attributes:
-                if type(node_attribute) == datetime:
+                if isinstance(node_attribute, datetime):
                     neo4j_datetime = DateTime(node_attribute.year, node_attribute.month, node_attribute.day, node_attribute.minute, node_attribute.second)
                     target_node_attributes.update({key, neo4j_datetime})
             target_node_labels = [label.capitalize() for label in target_node_labels]
@@ -146,15 +188,6 @@ class Neo4jManager():
                     edge_attributes.update({key, neo4j_datetime})
             edge_attributes['neo4j_collection'] = collection_name
 
-            self.merge_edge(source_node_labels, source_node_attributes, target_node_labels,
+            self.merge_edge_from_triples(source_node_labels, source_node_attributes, target_node_labels,
                     target_node_attributes, relation_type, edge_attributes, db)
-
-    def create_graph(self, collection_name: str, triples: List[dict]):
-        triples_df = pd.json_normalize(triples, max_level=0)
-        self.create_node(collection_name, triples_df['Subject'].values.tolist())
-        self.create_node(collection_name, triples_df['Object'].values.tolist())
-        self.generate_edges(collection_name, triples)
-        return
-    
-  
     
